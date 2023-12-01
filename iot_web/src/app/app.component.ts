12c793261ca7831b3df990f2edbc3562dabd624c
ax_abror_m3 <abror.mukhammadiev@axtechnology.uz>
2023-12-01 22:48:17 +0500

    feat: template and styling for turn on/off

---
+++ iot_web/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'iot_web';
+
+  lightStatus: string = 'Off';
+
+  turnOnLights() {
+    this.lightStatus = 'On';
+  }
+
+  turnOffLights() {
+    this.lightStatus = 'Off';
+  }
+}
